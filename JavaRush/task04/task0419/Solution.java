0017543a03b830df7b1ce3c9497c94ae5403b5eb
Andrew Nagiyev <andrew_nagiev09@mail.ru>
2019-02-28 23:59:09 +0300

    Java_JavaRush_JavaSyntax_Task04_7

---
+++ JavaRush/task04/task0419/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task04.task0419;
+
+/* 
+Максимум четырех чисел
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(bufferedReader.readLine());
+        int b = Integer.parseInt(bufferedReader.readLine());
+        int c = Integer.parseInt(bufferedReader.readLine());
+        int d = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println((a > b ? a : b) > (c > d ? c : d) ? (a > b ? a : b) : (c > d ? c : d));
+    }
+}
