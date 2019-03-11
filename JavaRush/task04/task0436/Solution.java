39f46a66d7ff0c33d2f021eedaa8245ba78f0505
Andrew Nagiyev <andrew_nagiev09@mail.ru>
2019-03-12 00:01:47 +0300

    Java_JavaRush_JavaSyntax_Task04_17

---
+++ JavaRush/task04/task0436/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task04.task0436;
+
+
+/* 
+Рисуем прямоугольник
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(bufferedReader.readLine());
+        int b = Integer.parseInt(bufferedReader.readLine());
+
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
+
+    }
+}
